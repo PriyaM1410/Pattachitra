55222191d29239a3b596ffd24d6723926e343edd
PriyadharshiniM1410 <m.priyadharshini141006@gmail.com>
2026-08-16 11:05:11 +0530

    feat: add testimonial schema to Sanity

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -2,11 +2,13 @@ import { type SchemaTypeDefinition } from 'sanity'
 import { contact } from './contact'
 import category from './category'
 import artwork from './artwork'
+import testimonial from './testimonial'
 
 export const schemaTypes = [
   category,
   artwork,
-  contact
+  contact,
+  testimonial
 ]
 // sanity.config.ts expects 'schema' export
 export const schema = {
